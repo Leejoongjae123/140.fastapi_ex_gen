127d99108d63c2f336849451396d9d4b8280bfde
ljj33 <ljj3347@naver.com>
2023-08-07 00:10:29 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,134 @@
+from fastapi import FastAPI
+from typing import List
+from starlette.middleware.cors import CORSMiddleware
+import model
+from db import session
+from model import PostingTable, Posting
+# from mangum import Mangum
+from typing import List, Optional
+from sqlalchemy import or_, and_
+from fastapi import FastAPI, HTTPException
+from mangum import Mangum
+from pydantic import BaseModel
+
+app = FastAPI() #앱을 생성한다
+handler=Mangum(app) #LAMBDA 배포용 MANGUM을 가져온다.
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# ----------API 정의------------
+# @app.get("/getProducts")
+# def read_users():
+#     users = session.query(UserTable).all()
+#     return users
+
+@app.get('/getProducts', response_model=List[Posting])
+async def read_items(keyword: Optional[str] = None, platform: Optional[str] = None,page: Optional[int] = 1, sortby: Optional[str] = None, sortorder: Optional[str] = None):
+    if keyword and platform:
+        print("키워드와 플랫폼이 함께 입력됨")
+        # 각각의 조건을 만들어줌
+        keyword_condition = PostingTable.title.contains(keyword)
+        platform_condition = PostingTable.platform == platform
+        # 조건들을 and_로 연결하여 모두 만족하는 레코드 검색
+        postings = session.query(PostingTable).filter(and_(keyword_condition, platform_condition))
+    elif keyword:
+        print("키워드만 입력됨")
+        keyword_condition = PostingTable.title.contains(keyword)
+        postings = session.query(PostingTable).filter(keyword_condition)
+    elif platform:
+        print("플랫폼만 입력됨")
+        platform_condition = PostingTable.platform == platform
+        postings = session.query(PostingTable).filter(platform_condition)
+    else:
+
+        print("키워드와 플랫폼이 모두 입력되지 않음")
+        postings = session.query(PostingTable)
+
+    # 정렬 기준에 따라 결과 소팅
+    if sortby:
+        if sortby == "dday":
+            sort_column = PostingTable.dday
+        elif sortby == "demandCount":
+            sort_column = PostingTable.demandCount
+        elif sortby == "applyCount":
+            sort_column = PostingTable.applyCount
+        else:
+            sort_column = None
+        if sort_column:
+            if sortorder == "dsc":
+                postings = postings.order_by(sort_column.desc())
+            else:
+                postings = postings.order_by(sort_column)
+    # 페이지 당 결과 개수
+    items_per_page = 40
+    # 전체 결과 개수
+    try:
+        postings = postings.offset((page - 1) * items_per_page).limit(items_per_page)
+    except:
+        postings=postings[:40]
+
+    # 페이지 번호에 따라 결과 제한
+
+    # User 모델로 변환
+    return [Posting(id=posting.id, platform=posting.platform, region=posting.region,dday=posting.dday,title=posting.title,demandCount=posting.demandCount,applyCount=posting.applyCount,imageUrl=posting.imageUrl,url=posting.url,myImage=posting.myImage) for posting in postings]
+
+@app.post('/search', response_model=List[Posting])
+async def read_items(keywords1: Optional[List[str]] = None,keywords2: Optional[List[str]] = None):
+    if keywords1 and keywords2:
+        print("and_keyword")
+        # 각각의 조건을 만들어줌
+        conditions1 = [PostingTable.title.contains(keyword) for keyword in keywords1]
+        combined_condition1 = or_(*conditions1)
+        conditions2 = [PostingTable.title.contains(keyword) for keyword in keywords2]
+        combined_condition2 = or_(*conditions2)
+        postings = session.query(PostingTable).filter(and_(combined_condition1, combined_condition2))
+
+    # 페이지 번호에 따라 결과 제한
+
+    # User 모델로 변환
+    return [Posting(id=posting.id, platform=posting.platform, region=posting.region,dday=posting.dday,title=posting.title,demandCount=posting.demandCount,applyCount=posting.applyCount,imageUrl=posting.imageUrl,url=posting.url,myImage=posting.myImage) for posting in postings]
+
+
+@app.post("/addProducts")
+# /user?name="이름"&age=10
+async def create_user(postings:List[Posting]):
+    for postingData in postings:
+        product = PostingTable()
+        product.platform = postingData.platform
+        product.region = postingData.region
+        product.dday = postingData.dday
+        product.title = postingData.title
+        product.demandCount = postingData.demandCount
+        product.applyCount = postingData.applyCount
+        product.imageUrl = postingData.imageUrl
+        product.url = postingData.url
+        product.myImage = postingData.myImage
+        session.add(product)
+
+    session.commit()
+
+    return {"status":'success','noPostings':len(postings)}
+
+# @app.put("/users")
+# async def update_users(users: List[User]):
+#     for i in users:
+#         user = session.query(UserTable).filter(UserTable.id == i.id).first()
+#         user.name = i.name
+#         user.price = i.price
+#         user.time = i.time
+#         session.commit()
+#
+#     return f"{users[0].name} updated..."
+
+
+@app.delete("/removeProducts")
+async def delete_users():
+    user = session.query(PostingTable).delete()
+    session.commit()
+    return {'status':'remove all'}
